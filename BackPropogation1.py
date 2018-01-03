f807d5d6ae99f6b57eb862b168a29cc1b7d63332
Arnab Sen <arnb.sen@hotmail.com>
2018-01-04 00:49:41 +0530

    Upadting BackPropogation

--- BackPropogation1.py
+++ BackPropogation1.py
@@ -1,6 +1,6 @@
-im1 = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/DB/input/in000001.png'
-im2 = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/DB/input/in000882.png'
-lb = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/DB/groundtruth/gt000882.png'
+im1 = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/IMAGE_DB/1cell.png'
+im2 = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/IMAGE_DB/2cell.png'
+lb = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/IMAGE_DB/ref1.png'
 from BackPropogation import *
 (dif, lbl, r) = twoDimImread(im1, im2, lb)
 lbl = convertImageToBinary(lbl)
