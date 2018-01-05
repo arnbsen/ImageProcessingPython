c148fd1ece27dd38c274238134ab5396f6ebe19e
arnbs <arnb.sen@hotmail.com>
2018-01-05 19:24:03 +0530

    Created Required Functions

---
+++ BackPropogation2.py
@@ -0,0 +1,4 @@
+from BackPropogation import *
+im1 = 'C:\\Users\\arnbs\\PycharmProjects\\ImageProcessingPython\\DB\\input\\in000001.png'
+im = 'C:\\Users\\arnbs\\PycharmProjects\\ImageProcessingPython\\DB\\input\\in'
+l = 'C:\\Users\\arnbs\\PycharmProjects\\ImageProcessingPython\\DB\\groundtruth\\gt'
