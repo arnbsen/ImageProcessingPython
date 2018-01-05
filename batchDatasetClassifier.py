bb7449a50083286e985c4cc083615b9736fc8936
Arnab Sen <arnb.sen@hotmail.com>
2018-01-06 00:00:30 +0530

    Upadting BackPropogation

--- batchDatasetClassifier.py
+++ batchDatasetClassifier.py
@@ -2,6 +2,7 @@ from change_knn import *
 model = model_load("kNNOfficek7.knndata")
 imext = '.jpg'
 gtext = '.pgm'
+""""
 for i in range(1,10):
     im1 = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/office/input/in000001.jpg'
     im2 = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/office/input/in00000' + str(i) + imext
@@ -9,6 +10,7 @@ for i in range(1,10):
     (out,r) = classifier(im1,im2,model)
     write_file(lb,out,r)
     print("Classifying Instance",i, sep=" ")
+    
 for i in range(10,100):
     im1 = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/office/input/in000001.jpg'
     im2 = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/office/input/in0000' + str(i) + imext
@@ -16,13 +18,15 @@ for i in range(10,100):
     (out,r) = classifier(im1,im2,model)
     write_file(lb,out,r)
     print("Classifying Instance",i, sep=" ")
-for i in range(100,1000):
+"""
+for i in range(600,1000):
     im1 = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/office/input/in000001.jpg'
     im2 = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/office/input/in000' + str(i) + imext
     lb = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/office/output/cl000' + str(i) + gtext
     (out,r) = classifier(im1,im2,model)
     write_file(lb,out,r)
     print("Classifying Instance",i, sep=" ")
+"""
 for i in range(1002,2051):
     im1 = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/office/input/in000001.jpg'
     im2 = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/office/input/in00' + str(i) + imext
@@ -30,3 +34,4 @@ for i in range(1002,2051):
     (out,r) = classifier(im1,im2,model)
     write_file(lb,out,r)
     print("Classifying Instance",i, sep=" ")
+"""
