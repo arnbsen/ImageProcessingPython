66eee4870e0fa1f3bb7d8c9aa39c39253059e787
Arnab Sen <arnb.sen@hotmail.com>
2018-01-13 11:54:09 +0530

    Upadting BackPropogation

--- BackPropogation1.py
+++ BackPropogation1.py
@@ -1,34 +1,13 @@
 im1 = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/DB/input/in000001.png'
 in2 = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/DB/input/in00'
 l = '/Users/arnabsen/Documents/IMAGEPROCESSING_PYTHON/DB/groundtruth/gt'
-from BackPropogation import *
+from MLPChange import *
 
 print("Training with two examples")
 im2 = in2 + '0882.png'
 lb = l + '000882.png'
-(dif, lbl, r) = twoDimImread(im1, im2, lb)
-lbl = convertImageToBinary(lbl)
-(wv, o1) = BackPropagation(dif,lbl,r,100,assignIntWeights(),0.001)
-im2 = in2 + '1598.png'
-lb = l + '001598.png'
-(dif, lbl, r) = twoDimImread(im1, im2, lb)
-lbl = convertImageToBinary(lbl)
-(wv, o2) = BackPropagation(dif,lbl,r,100,wv,0.001)
-im2 = in2 + '1307.png'
-(dif, lbl, r) = twoDimImread(im1, im2, lb)
-(o3,r)= BackPropagationClassifier(dif, r, wv)
-im2 = in2 + '1517.png'
-(dif, lbl, r) = twoDimImread(im1, im2, lb)
-(o4,r)= BackPropagationClassifier(dif, r, wv)
-im2 = in2 + '1547.png'
-(dif, lbl, r) = twoDimImread(im1, im2, lb)
-(o5,r)= BackPropagationClassifier(dif, r, wv)
-im2 = in2 + '1648.png'
-(dif, lbl, r) = twoDimImread(im1, im2, lb)
-(o6,r)= BackPropagationClassifier(dif, r, wv)
-im2 = in2 + '1598.png'
-(dif, lbl, r) = twoDimImread(im1, im2, lb)
-(o7,r)= BackPropagationClassifier(dif, r, wv)
-im2 = in2 + '1700.png'
-(dif, lbl, r) = twoDimImread(im1, im2, lb)
-(o8,r)= BackPropagationClassifier(dif, r, wv)
+(img1, img2, lbl, r) = twoDimImread(im1, im2, lb)
+(lbl, lblInv) = convertImageToBinary(lbl)
+misc.imsave('inv.png', lblInv)
+(wv, o) = BackPropagation(img1, img2, lbl, lblInv, r, 50, assignIntWeights(), 0.001, 0.001)
+out = Classifier(img1, img2, wv, r)
