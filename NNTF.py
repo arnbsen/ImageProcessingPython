f403bdb02a8c7c7a0ab0a55ddb85156f9f63bdb2
Arnab Sen <arnb.sen@hotmail.com>
2018-02-09 18:48:38 +0530

    Updating Support Library

--- NNTF.py
+++ NNTF.py
@@ -1,155 +1,24 @@
 import tflearn
-from tensorflow import DType
+from supportLibrary import *
 import numpy as np
 from scipy import misc
-import math
 
-
-def convertImageToBinary(lbl):
-    binImage = []
-    binImageInv = []
-    for i in range(0, len(lbl)):
-        tmp = []
-        tmp2 = []
-        for j in range(0, len(lbl[0])):
-            if lbl[i][j] == 0:
-                tmp.append(0)
-                tmp2.append(1)
-            else:
-                tmp.append(1)
-                tmp2.append(0)
-        binImage.append(tmp)
-        binImageInv.append(tmp2)
-    return binImage, binImageInv
-
-
-def windowCreator(i, j, h, w, arr):
-    window = []
-    for k in range(h):
-        for l in range(w):
-            try:
-                window.append(arr[i + h - 1][j + w - 1])
-            except IndexError:
-                print("i = ", i, " j = ", j)
-    return window
-
-
-def prepareData(im1, im2, lb):
-    # dif = abs(misc.imread(im1).astype(float)/255.0 - misc.imread(im2).astype(float)/255.0)
-    img1 = (misc.imread(im1).astype(float)).tolist()
-    img2 = (misc.imread(im2).astype(float)).tolist()
-    # dif = np.pad(dif, pad_width=10, mode='reflect').tolist()
-    lbl = misc.imread(lb).tolist()
-    binImage = np.array(lbl).astype(int).ravel().tolist()
-    # binImageInv = np.array(binImageInv).astype(float).ravel().tolist()
-    data = []
-    labels = []
-    for i in range(len(img1)):
-        for j in range(len(img1[0])):
-            data = data + [[abs(img1[i][j] - img2[i][j])]]
-            labels = labels + [[binImage[i + j] // 255]]
-    r = (len(img1), len(img1[0]))
-    data = np.array(data).tolist()
-    labels = np.array(labels).tolist()
-    return data, labels, r
-
-
-def prepareClusterData(im1, lb):
-    print("Preparing Data. Please Wait...")
-    dif = np.pad(misc.imread(im1).astype(float) / 255.0, mode='reflect', pad_width=(2, 2)).tolist()
-    print(len(dif), len(dif[0]))
-    binImage = (misc.imread(lb).astype(float) / 255.0).ravel()
-    binImageInv = abs(np.full(binImage.shape, 1.0) - binImage).tolist()
-    data = []
-    labels = []
-    for i in range(2, len(dif) - 2):
-        for j in range(2, len(dif[0]) - 2):
-            data = data + [windowCreator(i, j, 3, 3, dif)]
-            labels = labels + [[binImage[i + j], binImageInv[i + j]]]
-    r = (len(dif), len(dif[0]))
-    return data, labels, r
-
-
-def prepareDataAlternateNorm(im1, im2, lb):
-    r = misc.imread(im1).shape
-    img1 = misc.imread(im1).ravel().tolist()
-    min1 = min(img1)
-    max1 = max(img1)
-    img2 = misc.imread(im2).ravel().tolist()
-    min2 = min(img2)
-    max2 = max(img2)
-    binImage = abs(misc.imread(lb).astype(float) // 255).astype(int).ravel()
-    binImageInv = abs(np.full(binImage.shape, 1.0) - binImage).tolist()
-    img1 = np.array(img1)
-    img2 = np.array(img2)
-    binImage = binImage.tolist()
-    # Normalisation of data
-    img1 = ((img1 - np.full(img1.shape, min1)) / (max1 - min1))
-    img2 = ((img2 - np.full(img2.shape, min2)) / (max2 - min2))
-    img1 = img1.tolist()
-    img2 = img2.tolist()
-    data = []
-    labels = []
-    for i in range(len(img1)):
-        data = data + [[abs(img2[i] - img1[i])]]
-        labels = labels + [[binImage[i]]]
-    data = data
-    labels =labels
-    return data, labels, r
-
-
-def prepareDataInter(im1, im2, lb):
-    img1 = misc.imread(im1).astype(np.float32)
-    r = img1.shape
-    img2 = misc.imread(im2).astype(np.float32)
-    img1 = np.pad(img1, pad_width=(1, 1), mode='reflect').tolist()
-    img2 = np.pad(img2, pad_width=(1, 1), mode='reflect').tolist()
-    binImage = misc.imread(lb).astype(np.int64) // 255
-    binImageInv = abs(np.full(binImage.shape, 1) - binImage).tolist()
-    binImage = binImage.tolist()
-    data = []
-    labels = []
-    for i in range(1, r[0] + 1):
-        for j in range(1, r[1] + 1):
-            data = data + [[img1[i - 1][j - 1], img2[i - 1][j - 1], img1[i - 1][j], img2[i - 1][j], img1[i - 1][j + 1],
-                     img2[i - 1][j + 1], img1[i][j - 1], img2[i][j - 1], img1[i][j], img2[i][j],
-                     img1[i][j], img2[i][j + 1], img1[i + 1][j - 1], img2[i + 1][j - 1], img1[i + 1][j], img2[i + 1][j],
-                     img1[i + 1][j + 1], img2[i + 1][j + 1]]]
-
-    for i in range(r[0]):
-        for j in range(r[1]):
-            labels = labels + [[binImage[i][j]]]
-    return np.array(data, dtype=np.float32).tolist(), np.array(labels, dtype=np.float32).tolist(), r
 
 def netInit():
     print("Initialising the net......")
-    net = tflearn.input_data(shape=[None, 1])
+    net = tflearn.input_data(shape=[None, 9])
+    net = tflearn.fully_connected(net, 18, activation='sigmoid')
+    net = tflearn.fully_connected(net, 10, activation='sigmoid')
     net = tflearn.fully_connected(net, 5, activation='sigmoid')
     net = tflearn.fully_connected(net, 1, activation='sigmoid')
-    net = tflearn.regression(net, optimizer='sgd', loss='mean_square')
+    net = tflearn.regression(net, optimizer='sgd', loss='binary_crossentropy', learning_rate=0.6)
     model = tflearn.DNN(net)
     return model
 
 
 def trainData(model, data, labels):
-    model.fit(data, labels, show_metric=True, n_epoch=12000)
+    model.fit(data, labels, show_metric=True, n_epoch=5000)
     return model
-
-
-def writeImage(model, data, r):
-    output = model.predict(data)
-    output = output.tolist()
-    temp = []
-    for i in range(1, r[0]+1):
-        itemp = []
-        for j in range(1, r[1]+1):
-            if output[i + j][0] < output[i + j][1]:
-                itemp = itemp + [0]
-            else:
-                itemp = itemp + [255]
-        temp = temp + [itemp]
-    temp = np.array(temp).astype(np.uint8)
-    misc.imsave('disaster.png', temp)
 
 
 im1 = '/Users/arnabsen/PycharmProjects/ImageProcessingPython/IMAGE_DB/cell1small.png'
@@ -160,7 +29,7 @@ print("Training with two examples")
 im2 = in2  # +'0882.png'
 lb = l  # + '0882.png'
 
-(data, labels, r) = prepareData(im1, im2, lb)
+(data, labels, r) = prepareDataStdDev(im1, im2, lb)
 model = netInit()
 model = trainData(model, data, labels)
-
+writeImage(model, data, r)
--- supportLibrary.py
+++ supportLibrary.py
@@ -1,6 +1,7 @@
 # Some of the basic functions required for prepossessing for Image Processing
 import numpy as np
 import statistics
+
 from scipy import misc
 
 
@@ -60,6 +61,171 @@ def pearsonCorrelationFilter(data1, data2, r, windowWidth):
 
         output = output + [temp]
     return output
+
+def normalisation(data):
+    min1 = 999999
+    max1 = -999999
+    for i in range(len(data)):
+        min1 = min(min1, min(data[i]))
+        max1 = max(max1, max(data[i]))
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            data[i][j] = round((data[i][j] - min1) / (max1 - min1), 2)
+    return data
+
+def convertImageToBinary(lbl):
+    binImage = []
+    binImageInv = []
+    for i in range(0, len(lbl)):
+        tmp = []
+        tmp2 = []
+        for j in range(0, len(lbl[0])):
+            if lbl[i][j] == 0:
+                tmp.append(0)
+                tmp2.append(1)
+            else:
+                tmp.append(1)
+                tmp2.append(0)
+        binImage.append(tmp)
+        binImageInv.append(tmp2)
+    return binImage, binImageInv
+
+
+def windowCreator(i, j, h, w, arr):
+    window = []
+    for k in range(h):
+        for l in range(w):
+            try:
+                window.append(arr[i + k - 1][j + l - 1])
+            except IndexError:
+                print("i = ", i, " j = ", j)
+    return window
+
+
+def prepareData(im1, im2, lb):
+    # dif = abs(misc.imread(im1).astype(float)/255.0 - misc.imread(im2).astype(float)/255.0)
+    img1 = (misc.imread(im1).astype(float)).tolist()
+    img2 = (misc.imread(im2).astype(float)).tolist()
+    # dif = np.pad(dif, pad_width=10, mode='reflect').tolist()
+    lbl = misc.imread(lb).tolist()
+    binImage = np.array(lbl).astype(int).ravel().tolist()
+    # binImageInv = np.array(binImageInv).astype(float).ravel().tolist()
+    data = []
+    labels = []
+    for i in range(len(img1)):
+        for j in range(len(img1[0])):
+            data = data + [[abs(img1[i][j] - img2[i][j])]]
+            labels = labels + [[binImage[i + j] // 255]]
+    r = (len(img1), len(img1[0]))
+    data = np.array(data).tolist()
+    labels = np.array(labels).tolist()
+    return data, labels, r
+
+
+def prepareClusterData(im1, lb):
+    print("Preparing Data. Please Wait...")
+    dif = np.pad(misc.imread(im1).astype(float) / 255.0, mode='reflect', pad_width=(2, 2)).tolist()
+    print(len(dif), len(dif[0]))
+    binImage = (misc.imread(lb).astype(float) / 255.0).ravel()
+    binImageInv = abs(np.full(binImage.shape, 1.0) - binImage).tolist()
+    data = []
+    labels = []
+    for i in range(2, len(dif) - 2):
+        for j in range(2, len(dif[0]) - 2):
+            data = data + [windowCreator(i, j, 3, 3, dif)]
+            labels = labels + [[binImage[i + j], binImageInv[i + j]]]
+    r = (len(dif), len(dif[0]))
+    return data, labels, r
+
+
+def prepareDataAlternateNorm(im1, im2, lb):
+    r = misc.imread(im1).shape
+    img1 = misc.imread(im1).ravel().tolist()
+    min1 = min(img1)
+    max1 = max(img1)
+    img2 = misc.imread(im2).ravel().tolist()
+    min2 = min(img2)
+    max2 = max(img2)
+    binImage = abs(misc.imread(lb).astype(float) // 255).astype(int).ravel()
+    binImageInv = abs(np.full(binImage.shape, 1.0) - binImage).tolist()
+    img1 = np.array(img1)
+    img2 = np.array(img2)
+    binImage = binImage.tolist()
+    # Normalisation of data
+    img1 = ((img1 - np.full(img1.shape, min1)) / (max1 - min1))
+    img2 = ((img2 - np.full(img2.shape, min2)) / (max2 - min2))
+    img1 = img1.tolist()
+    img2 = img2.tolist()
+    data = []
+    labels = []
+    for i in range(len(img1)):
+        data = data + [[abs(img2[i] - img1[i])]]
+        labels = labels + [[binImage[i]]]
+    data = data
+    labels =labels
+    return data, labels, r
+
+
+def prepareDataInter(im1, im2, lb):
+    img1 = misc.imread(im1).astype(np.float32)
+    r = img1.shape
+    img2 = misc.imread(im2).astype(np.float32)
+    img1 = np.pad(img1, pad_width=(1, 1), mode='reflect').tolist()
+    img2 = np.pad(img2, pad_width=(1, 1), mode='reflect').tolist()
+    binImage = misc.imread(lb).astype(np.int64) // 255
+    binImageInv = abs(np.full(binImage.shape, 1) - binImage).tolist()
+    binImage = binImage.tolist()
+    data = []
+    labels = []
+    for i in range(1, r[0] + 1):
+        for j in range(1, r[1] + 1):
+            data = data + [[img1[i - 1][j - 1], img2[i - 1][j - 1], img1[i - 1][j], img2[i - 1][j], img1[i - 1][j + 1],
+                     img2[i - 1][j + 1], img1[i][j - 1], img2[i][j - 1], img1[i][j], img2[i][j],
+                     img1[i][j], img2[i][j + 1], img1[i + 1][j - 1], img2[i + 1][j - 1], img1[i + 1][j], img2[i + 1][j],
+                     img1[i + 1][j + 1], img2[i + 1][j + 1]]]
+
+    for i in range(r[0]):
+        for j in range(r[1]):
+            labels = labels + [[binImage[i][j]]]
+    return np.array(data, dtype=np.float32).tolist(), np.array(labels, dtype=np.float32).tolist(), r
+def writeImage(model, data, r):
+    output = model.predict(data)
+    output = output.tolist()
+    temp = []
+    for i in range(1, r[0]+1):
+        itemp = []
+        for j in range(1, r[1]+1):
+            if output[i + j][0] < 0.1:
+                itemp = itemp + [0]
+            else:
+                itemp = itemp + [255]
+        temp = temp + [itemp]
+
+    temp = np.array(temp).astype(np.uint8)
+    misc.imsave('disaster.png', temp)
+
+def prepareDataStdDev(im1, im2, lb):
+    dif = normalisation(abs(misc.imread(im1).astype(float) - misc.imread(im2).astype(float)).tolist())
+    lbl = (misc.imread(lb).astype(int) // 255).ravel().reshape(-1, 1).tolist()
+    difstd = np.pad(normalisation(stdDevFilter(dif, 3)), pad_width=(1, 1), mode='reflect')
+    dif = np.pad(dif, pad_width=(1, 1), mode='reflect').tolist()
+    t = []
+    for i in range(1, len(dif)-1):
+        for j in range(1, len(dif[0])-1):
+            t = t + [windowCreator(i, j, 3, 3, difstd)] #+ windowCreator(i, j, 3, 3, difstd)]
+    r = (len(dif)-2, len(dif[0])-2)
+    return t, lbl, r
+
+def normalisation2(im1, im2, lb):
+    it1 = misc.imread(im1).ravel().tolist()
+    it2 = misc.imread(im2).ravel().tolist()
+    var1 = statistics.variance(it1)
+    var2 = statistics.variance(it2)
+    mean1 = statistics.mean(it1)
+    mean2 = statistics.mean(it2)
+    dif = abs(misc.imread(im1) - (var1/var2)*(misc.imread(im2) - mean2) + mean1)
+    lbl = (misc.imread(lb).astype(int) // 255).ravel().reshape(-1, 1).tolist()
+
 
 
 """"
