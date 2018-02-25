b303b55242cd504b214ff4c14b12254518d4bba8
Arnab Sen <arnb.sen@hotmail.com>
2018-02-25 11:28:24 +0530

    Upadting BackPropogation

--- BackPropogation1.py
+++ BackPropogation1.py
@@ -1,6 +1,6 @@
-im1 = '/Users/arnabsen/PycharmProjects/ImageProcessingPython/IMAGE_DB/cell1.png'
-in2 = '/Users/arnabsen/PycharmProjects/ImageProcessingPython/IMAGE_DB/cell2.png'
-l = '/Users/arnabsen/PycharmProjects/ImageProcessingPython/IMAGE_DB/changeSD.png'
+im1 = '/Users/arnabsen/PycharmProjects/ImageProcessingPython/IMAGE_DB/cell1small2.png'
+in2 = '/Users/arnabsen/PycharmProjects/ImageProcessingPython/IMAGE_DB/cell2small2.png'
+l = '/Users/arnabsen/PycharmProjects/ImageProcessingPython/IMAGE_DB/gtsmall2.png'
 from NN1 import *
 
 print("Training......")
@@ -9,6 +9,6 @@ lb = l # + '000882.png'
 (img1, lbl, r) = twoDimImread(im1, im2, lb)
 (lbl, lblInv) = convertImageToBinary(lbl)
 misc.imsave('inv.png', lblInv)
-(wv, o) = BackPropagation(normalisation(img1.tolist()), lbl, lblInv, r, noOfEpochs=5000, wv= assignIntWeights(), l_rate= 0.6, alpha=1)
+(wv, o) = BackPropagation(normalisation(img1.tolist()), lbl, lblInv, r, noOfEpochs=10000, wv= assignIntWeights(), l_rate=0.006, alpha=1)
 o1 = writeImage2(o,r)
 print(errorCalc(output=o1, lbl=lbl, r=r))
